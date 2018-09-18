1d232c1fd30562021602f2ebf9fc726afade7266
Daniel da Silva <daniel@developmentseed.org>
2018-09-18 09:43:21 +0100

    Add description

--- app/assets/scripts/config/production.js
+++ app/assets/scripts/config/production.js
@@ -4,7 +4,7 @@ export default {
   environment: 'production',
   appTitle: 'Rural Accessibility Hub',
   appSubtitle: 'Powered by Rural Accessibility Map',
-  appDescription: 'No description yet.',
+  appDescription: 'The Rural Accessibility Hub is the place to showcase and share accessibility analysis generated using the RAM toolset.',
   baseurl: 'https://rah.surge.sh',
   mbtoken: null
 }
